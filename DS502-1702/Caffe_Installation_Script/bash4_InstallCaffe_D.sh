4a7d29b4b6a95f7fd065509f193f688f36d69ded
Oliverwong369 <wjy347@126.com>
2017-07-02 07:42:32 +0800

    Fix some bugs and add new bash script to set environment variable (PATH)

--- DS502-1702/Caffe_Installation_Script/bash4_InstallCaffe_D.sh
+++ DS502-1702/Caffe_Installation_Script/bash4_InstallCaffe_D.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export TERM=xterm
+
 clear
 
 echo "======================================="
@@ -30,7 +32,10 @@ cd ~/caffe/build && make pytest
 echo "======================================="
 echo -e "step6:\n[NOTE]caffe version："
 
-cd ~/caffe/python && python -c "import caffe; print caffe.__version__"
+cd ~/caffe/python && echo " --> " && python -c "import caffe; print caffe.__version__"
+
+echo "======================================="
+echo -e "step7:\n[NOTE]如成功编译、安装并测试成功，上面会显示 caffe 的版本号。例如：1.0.0。\n请在新窗口中继续执行脚本 bash5_InstallCaffe-E.sh"
 
 exit
 
---
+++ DS502-1702/Caffe_Installation_Script/bash5_InstallCaffe_E.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export TERM=xterm
+
+clear
+
+sudo -S ./binary
+
+echo "======================================="
+echo "step1: "
+
+sudo rm -r ~/python
+
+echo "======================================="
+echo "step2: "
+
+mkdir ~/python
+
+echo "======================================="
+echo "step3: "
+
+cp -R ~/caffe/python/caffe ~/python
+
+echo "======================================="
+echo "step4: "
+
+echo -e "\n# added the caffe python module directory to \$PYTHONPATH" >> ~/.bash_profile
+
+echo "======================================="
+echo "step5: "
+
+echo -e "export PYTHONPATH=~/python:\$PYTHONPATH" >> ~/.bash_profile
+
+echo "======================================="
+echo "step6: "
+
+source ~/.bash_profile
+
+echo "======================================="
+echo -e "step7:\n[NOTE]Try type python anywhere and import caffe.\ncaffe version："
+
+cd ~/ && echo " --> " && python -c "import caffe; print caffe.__version__"
+
+echo "======================================="
+echo -e "step7:\n[NOTE]如环境变量设置正确，上面会显示 caffe 的版本号。例如：1.0.0。"
+
+exit
