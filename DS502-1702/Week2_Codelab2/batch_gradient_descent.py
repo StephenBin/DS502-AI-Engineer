38b09b364fb78331e52aa79c748225c0c8deab27
Jason Li <lzzduke06@gmail.com>
2017-07-11 13:41:49 +0800

    upload homework 2

---
+++ DS502-1702/Week2_Codelab2/batch_gradient_descent.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+# 构造训练数据
+x = np.arange(0., 10., 0.2)
+m = len(x)  # 训练数据点数目
+x0 = np.full(m, 1.0)
+input_data = np.vstack([x0, x]).T  # 将偏置b作为权向量的第一个分量
+target_data = 2 * x + 5 + np.random.randn(m)
+
+# 两种终止条件
+loop_max = 10000  # 最大迭代次数(防止死循环)
+epsilon = 1e-3
+
+# 初始化权值
+np.random.seed(0)
+w = np.random.randn(2)
+# w = np.zeros(2)
+
+alpha = 0.001  # 步长(注意取值过大会导致振荡,过小收敛速度变慢)
+diff = 0.
+error = np.zeros(2)
+count = 0  # 循环次数
+finish = 0  # 终止标志
+error_list = []
+
+# -----------------------------------------------批量梯度下降法-----------------------------------------------------------
+while count < loop_max:
+    count += 1
+
+    # 标准梯度下降是在权值更新前对所有样例汇总误差，而随机梯度下降的权值是通过考查某个训练样例来更新的
+    # 在标准梯度下降中，权值更新的每一步对多个样例求和，需要更多的计算
+    sum_m = np.zeros(2)
+    for i in range(m):
+        dif = (np.dot(w, input_data[i]) - target_data[i]) * input_data[i]
+        sum_m = sum_m + dif  # 当alpha取值过大时,sum_m会在迭代过程中会溢出
+
+    w = w - alpha * sum_m  # 注意步长alpha的取值,过大会导致振荡
+    error_list.append(np.sum(sum_m)**2)
+    # 判断是否已收敛
+    if np.linalg.norm(w - error) < epsilon:
+        finish = 1
+        break
+    else:
+        error = w
+print 'loop count = %d' % count, '\tw:[%f, %f]' % (w[0], w[1])
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# check with scipy linear regression
+slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x, target_data)
+print 'intercept = %s slope = %s' % (intercept, slope)
+
+plt.plot(range(len(error_list[0:100])), error_list[0:100])
+plt.show()
+
+plt.plot(x, target_data, 'k+')
+plt.plot(x, w[1] * x + w[0], 'r')
+plt.show()
--- DS502-1702/homework/README.md
+++ DS502-1702/homework/README.md
@@ -1,1 +1,17 @@
 # DS502-1702 AI工程师直通车课程作业
+
+## Week 1 作业:
+
+对adult数据文件进行数据预处理，数据包含字符串格式以及缺失数据。可以自行选择模型进行训练。
+
+请提交作业至 Issue 中Week1页面。
+
+Solution: 参见week1_homework_solution.py文件。
+
+## Week 2 作业:
+
+根据batch_gradient_descent.py 和 stochastic_gradient_descent.py 两个文件，自己编写 mini_batch_gradient_descent.py 实现 Mini-batch Gradient Descent。数据可以修改为pima_indians_diabetes.txt数据，测试模型可以自己选择。
+
+**有兴趣的同学可以加上Momentum，Adagrad等优化方法，绘制loss function图像观察梯度下降效果。**
+
+请提交作业至 Issue 中Week2页面。
