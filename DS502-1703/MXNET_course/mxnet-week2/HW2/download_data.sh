155f6cbbae32895fe868d46964c2482d5ab3ed29
Xuan TAN <xuan.tan@workday.com>
2018-01-24 15:27:09 -0800

    delete old repo

--- DS502-1703/MXNET_course/mxnet-week2/HW2/download_data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://data.mxnet.io/data/cifar10/cifar10_val.rec .
-wget http://data.mxnet.io/data/cifar10/cifar10_train.rec .
-wget http://data.mxnet.io/data/cifar10/cifar10_test.rec .
